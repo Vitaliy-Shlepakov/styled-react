ce33960a9fcc019577846f180f43ca1d011a3510
Vik <shlepakov58@gmail.com>
2021-03-25 09:46:22 +0300

    media query mixin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { css } from "styled-components";
+
+//media queries
+
+const size = {
+    $mobile: 768,
+    $tablet: 992,
+    $desktop: 1280,
+}
+
+const maxWidth = Object.keys(size).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+        @media(max-width: ${size[label]}px) {
+            ${css(...args)}
+    `;
+    return acc;
+}, {});
+
+//
+
+const Heading = styled.h1`
+  font-size: 2rem;
+  
+  ${maxWidth.$tablet`
+    color: red;
+  `}
+`;
+
+const Button = styled.button`
+    background-color: indigo;
+    padding: 5px 20px;
+    border-radius: 4px;
+    border: none;
+    color: #fff;
+    background: indigo;
+`;
+
+const CancelButton = styled(Button)`
+    background: tomato;
+`;
+
+const AppWrapper = styled.div`
+    background: #ddd;
+    min-height: 100vh;
+    
+    ${Button} {
+        margin: 2rem;
+    };
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Heading>Heading text</Heading>
+        <CancelButton>
+            Button
+        </CancelButton>
+        <Button>
+            Save
+        </Button>
+        <Fake
+            className={'TEST'}
+        />
+    </AppWrapper>
   );
-}
+};
+
+const Fake = ({className}) => <div className={className}>
+    <h2>I am fake component</h2>
+</div>
+
+
 
 export default App;
